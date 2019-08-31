243b254d86297696a0838ca792ccd806086e8b77
Boris Chen <boris.chen.czy@gmail.com>
2019-08-31 22:54:30 +0800

    Initial Commit

---
+++ Concentration3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration3
+//
+//  Created by Boris Chen on 31/8/2019.
+//  Copyright © 2019 Boris Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
